bbe910be5862ca978b091a526dc413e8f0d4a27c
mintuta <mintefikr4@gmail.com>
2022-12-01 02:06:14 -0600

    hire method got implemented

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,13 +1,17 @@
 package com.aa.act.interview.org;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public abstract class Organization {
 
 	private Position root;
+	private List<Position> positionList= new ArrayList<>();
 	
 	public Organization() {
 		root = createOrganization();
+		organizationPositionsList(root);
 	}
 	
 	protected abstract Position createOrganization();
@@ -21,9 +25,39 @@ public abstract class Organization {
 	 */
 	public Optional<Position> hire(Name person, String title) {
 		//your code here
+		Optional<Position> position = getPosition(title);
+		if(!position.isPresent())
+			return Optional.empty();
+		 Position pos = position.get();
+		if(pos.isFilled())
+			throw new OrganizationalHireException("This position is already filled, try looking another position");
+		Employee employee= new Employee(generateCustomerIdentifier(),person);
+		pos.setEmployee(Optional.of(employee));
+		return Optional.of(pos);
+	}
+    public Optional<Position> getPosition(String title){
+		for(Position p:positionList){
+			if(p.getTitle().equalsIgnoreCase(title)){
+				return Optional.of(p);
+			}
+
+		}
 		return Optional.empty();
 	}
 
+	public void organizationPositionsList(Position position){
+		Optional<Position> pos=Optional.empty();
+		positionList.add(position);
+		for(Position p : position.getDirectReports()) {
+			positionList.add(p);
+			organizationPositionsList(p);
+		}
+
+	}
+   public int generateCustomerIdentifier(){
+	   int rand = (int)Math.floor(Math.random()*(1000-100+1)+100);
+	   return rand;
+   }
 	@Override
 	public String toString() {
 		return printOrganization(root, "");
---
+++ java/com/aa/act/interview/org/OrganizationalHireException.java
@@ -0,0 +1,8 @@
+package com.aa.act.interview.org;
+
+public class OrganizationalHireException extends RuntimeException{
+
+    public OrganizationalHireException(final String msg){
+        super(msg);
+    }
+}
