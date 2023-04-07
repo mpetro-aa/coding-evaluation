fa415e3b075aad38c6eb1cf82be46fbac3ccbb33
BCColton <99459995+BCColton@users.noreply.github.com>
2023-04-06 19:56:50 -0700

    Added hire and findPosition methods
    Contributed a depth-first findPosition method that feeds the found result to the hire method. The Hire method then uses this result to update the organization or returns and empty position having done nothing.

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -21,8 +21,41 @@ public abstract class Organization {
 	 */
 	public Optional<Position> hire(Name person, String title) {
 		//your code here
+		Position result = findPosition(root, title);
+		
+		// Checks the result and returns an Optional<Employee> if one was found.
+		if (result != null) {
+			result.setEmployee(Optional.of(new Employee(0, person)));
+			return Optional.of(result);
+		}
+		
+		// Target was not found by the search so and empty optional is returned.
 		return Optional.empty();
 	}
+	
+	/*
+	 * Finds the position in the organization tree who's title matches the given String
+	 * using a depth-first tree traversal
+	 * 
+	 * @param target
+	 * @param title
+	 * @return the wanted position or null if not found.
+	 */
+	public Position findPosition(Position target, String title) {
+		
+		if (target.getTitle().equals(title)) {
+			return target;
+		} else if (!target.getDirectReports().isEmpty()) {			
+			for(Position position : target.getDirectReports()) {
+				Position newTarget = findPosition(position, title);
+				if (newTarget != null)
+					return newTarget;
+			}
+		}
+		
+		return null;
+	}
+		
 
 	@Override
 	public String toString() {
