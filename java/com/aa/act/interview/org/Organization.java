930db31cbdd89e5752aafde6523dfb84b017b4b1
Alan Autrand <alan.autrand@gmail.com>
2023-06-06 08:38:26 -0700

    Fixing hire function by adding a recursive traversal of the organization tree.

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -5,7 +5,8 @@ import java.util.Optional;
 public abstract class Organization {
 
 	private Position root;
-	
+	private int id = 1;
+
 	public Organization() {
 		root = createOrganization();
 	}
@@ -20,8 +21,40 @@ public abstract class Organization {
 	 * @return the newly filled position or empty if no position has that title
 	 */
 	public Optional<Position> hire(Name person, String title) {
-		//your code here
+		// assign employee if title is CEO/root
+		if (root.getTitle().equals(title)) {
+			return assignEmployee(root, person);
+		} else {
+			// or visit direct reports
+			return hire(root, person, title);
+		}
+	}
+
+	private Optional<Position> hire(Position currentPosition, Name person, String title) {
+		// visit all direct reports
+		for (Position pos : currentPosition.getDirectReports()) {
+			if (pos.getTitle().equals(title)) {
+				// Assign employee if title matches
+				return assignEmployee(pos, person);
+			} else if (!pos.getDirectReports().isEmpty()) {
+				// Recursively visit direct reports
+				Optional<Position> newPos = hire(pos, person, title);
+				if (newPos.isPresent()) {
+					return newPos;
+				}
+			}
+		}
 		return Optional.empty();
+	}
+
+	private Optional<Position> assignEmployee(Position position, Name person) {
+		if (!position.isFilled()) {
+			position.setEmployee(Optional.of(new Employee(id, person)));
+			id += 1;
+			return Optional.of(position);
+		} else {
+			throw new IllegalArgumentException("Position is already filled.");
+		}
 	}
 
 	@Override
